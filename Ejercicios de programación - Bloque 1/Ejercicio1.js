a48237cd0acc6195a1d0a9bbd906623765ebb347
Andrés Camilo Piratova Pineda <azprodiyi@gmail.com>
2024-08-03 17:53:35 -0500

    Ejercicio1

---
+++ Ejercicios de programación - Bloque 1/Ejercicio1.js
@@ -0,0 +1,23 @@
+'use strick'
+/*
+JS que pida dos números y nos diga cual es el mayor, el menor O si son iguales 
+*/
+
+var numero1 = parseInt(prompt('Introduce el primer numero', 0));
+var numero2 = parseInt(prompt('Introduce el segundo numero', 0));
+
+while (numero1 <= 0 || numero2 <= 0 || isNaN(numero1) || isNaN(numero2)) {
+    alert("Los datos fueron ingresados mal se volverán a solicitar")
+    numero1 = parseInt(prompt('Introduce el primer numero', 0));
+    numero2 = parseInt(prompt('Introduce el segundo numero', 0));
+}
+
+if (numero1 == numero2) {
+    alert("Los números son iguales")
+} else if (numero1 > numero2) {
+    alert("El numero mayor es: " + numero1 + ", y el numero menor es: " + numero2);
+} else if (numero1 < numero2) {
+    alert("El numero mayor es: " + numero2 + ", y el numero menor es: " + numero1);
+} else {
+    alert("Introduce un numero valido")
+}
